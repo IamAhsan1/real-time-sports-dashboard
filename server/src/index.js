60272be367e0dc4c2b0314c01fe6560e1eae7eb6
Ahsan Khan <iamahsan080@gmail.com>
2026-09-09 18:01:22 +0500

    Implement Matches get and post endpoints

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,10 @@
 import express from "express";
-
+import matchRouter from "./routes/matches.js";
 const app=express();
 app.use(express.json());    
 
-
-app.get("/Auth_System", (req,res)=>{
-    res.send("Auth_System is working");
-});
+app.use("/matches",matchRouter);
+app.use("/matches",matchRouter);
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
